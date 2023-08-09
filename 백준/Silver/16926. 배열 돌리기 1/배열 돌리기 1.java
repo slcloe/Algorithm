f3e9eb42bf303bf66f60dc0b23a988d12b9e5aa5
slcloe <67732600+slcloe@users.noreply.github.com>
2023-08-09 09:30:12 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 512 ms, Memory: 35180 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -4,27 +4,57 @@ import java.util.*;
 public class Main {
     static int N, M , r;
     static int[][] arr;
+    static boolean[][] visited;
+    static boolean[][] visited1;
     static StringBuilder sb = new StringBuilder();
+    static ArrayDeque<Integer> queue = new ArrayDeque<>();
 
     static void rotate(){
-        int min = (N > M) ? M/2 : N/2;
-        int[][] arr_copy = new int[N][M];
-        for (int i = 0; i < arr_copy.length; i++) {
-            arr_copy[i]= arr[i].clone();
+        int circle = (N > M) ? M / 2 : N / 2;
+        for (int i = 0; i < circle; i++) {
+            rotateOneCircle(i, i);
         }
-        for (int i = 0; i < min; i++){
-            int row = N - (2 * i) - 1;
-            int col = M - (2 * i) - 1;
-            for (int j=0;j<col;j++){
-                arr_copy[i][i + j] = arr[i][i + j + 1];
-                arr_copy[N - 1 - i][i + j + 1] = arr[N - 1 - i][i + j];
-            }
-            for (int j=0;j<row;j++){
-                arr_copy[j + i + 1][i] = arr[j + i][i];
-                arr_copy[i + j][M - 1 - i] = arr[i + j + 1][M - 1 - i];
+    }
+
+    static void rotateOneCircle(int x, int y){
+        int dx[] = {0, 1, 0, -1, 0};
+        int dy[] = {0, 0, 1, 0, -1};
+        queue = new ArrayDeque<>();
+
+        int tx = x - 1;
+        int ty = y;
+        for(int i = 0; i < 4; i++){
+            tx = tx - dx[i] + dx[i + 1];
+            ty = ty - dy[i] + dy[i + 1];
+            while (isRange(tx, ty) && !visited[tx][ty]){
+                visited[tx][ty] = true;
+                queue.offer(arr[tx][ty]);
+                tx = tx + dx[i + 1];
+                ty = ty + dy[i + 1];
             }
         }
-        arr = arr_copy;
+
+        for (int i = 0; i < r; i++) {
+            int tmp = queue.pollLast();
+            queue.offerFirst(tmp);
+        }
+        tx = x - 1;
+        ty = y;
+        for(int i = 0; i < 4; i++){
+            tx = tx - dx[i] + dx[i + 1];
+            ty = ty - dy[i] + dy[i + 1];
+            while (isRange(tx, ty) && !visited1[tx][ty]){
+                visited1[tx][ty] = true;
+                arr[tx][ty] = queue.poll();
+                tx = tx + dx[i + 1];
+                ty = ty + dy[i + 1];
+            }
+        }
+    }
+
+    static boolean isRange(int x, int y){
+        if (0 <= x && x < N && 0 <= y && y < M) return true;
+        return false;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,15 +64,15 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         arr= new int[N][M];
+        visited = new boolean[N][M];
+        visited1 = new boolean[N][M];
         for (int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine(), " ");
             for (int j=0;j<M;j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i=0;i<r;i++){
-            rotate();
-        }
+        rotate();
         for(int[] arr1 : arr){
             for(int arr2: arr1){
                 sb.append(arr2).append(" ");
