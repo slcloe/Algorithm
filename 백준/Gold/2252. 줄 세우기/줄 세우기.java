8723c8d783c04daa9b4c550ec06e3cff84000bae
slcloe <67732600+slcloe@users.noreply.github.com>
2023-08-22 11:19:25 +0900

    [Gold III] Title: 줄 세우기, Time: 468 ms, Memory: 43508 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,40 +1,47 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+import java.util.*;
+import java.io.*;
 public class Main {
+
+
     static int N, M;
     static List<Integer>[] g;
-    static ArrayDeque<Integer> s;
-    static boolean[] v;
-
-    static void dfs(int i){
-        v[i] = true;
-        for (int j : g[i]) {
-            if (!v[j]) dfs(j);
+    static int[] indegree;
+    static StringBuilder sb = new StringBuilder();
+    static void topologicalSort(){
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        for (int i = 1; i < N + 1 ; i++) {
+            if (indegree[i] == 0) q.offer(i);
         }
-        s.push(i); // 여기가 정답
+        while(!q.isEmpty()){
+            int i = q.poll();
+            sb.append(i).append(" ");
+            for(int j : g[i]){
+                if (--indegree[j] == 0) q.offer(j);
+            }
+        }
+
     }
-    public static void main(String[] args) throws Exception{
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        M = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         g = new List[N + 1];
         for (int i = 0; i < N + 1; i++) {
             g[i] = new ArrayList<>();
         }
-        s = new ArrayDeque<>();
-        v = new boolean[N + 1];
+        indegree = new int[N + 1];
         for (int i = 0; i < M; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             g[a].add(b);
+            indegree[b]++;
         }
-        for (int i = 1; i < N + 1; i++) {
-            if (!v[i]) dfs(i);
-        }
-        while(!s.isEmpty()) System.out.print(s.pop () + " ");
-        sc.close();
+        topologicalSort();
+        System.out.println(sb.toString());
+        br.close();
     }
 }
