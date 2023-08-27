32a79bbeb715aad3b56b94e76ca4fca1282cc2be
slcloe <67732600+slcloe@users.noreply.github.com>
2023-08-27 20:09:05 +0900

    [Gold II] Title: 청소년 상어, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/19236. 청소년 상어/청소년 상어.java
+++ 백준/Gold/19236. 청소년 상어/청소년 상어.java
@@ -2,88 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-
     static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
     static ArrayList<int[]> fishInfo = new ArrayList<>();
-
     static int[][] arr = new int[4][4];
-    static int[] sharkPos = {0, 0};
     static int max = 0;
 
-    static void dfs(int[][] map, ArrayList<int[]> fishSet, int dinner, int depth){
-//        System.out.println("depth : " + depth);
-//        System.out.println(dinner);
-//        System.out.println("before");
-//        for(int[] arr1 : map){
-//            System.out.println(Arrays.toString(arr1));
-//        }
-//        System.out.println();
-//        for(int[] arr1 : fishSet){
-//            System.out.println(Arrays.toString(arr1));
-//        }
-//        System.out.println();
-        moveFish(map, fishSet);
-//        System.out.println("after");
-//        for(int[] arr1 : map){
-//            System.out.println(Arrays.toString(arr1));
-//        }
-//        System.out.println();
-
-        int x = fishSet.get(17)[1]; // shark position
-        int y = fishSet.get(17)[2]; // shark position
+    static void dfs(int[][] map, ArrayList<int[]> fishSet, int dinner){
+        int tx = fishSet.get(17)[1];
+        int ty = fishSet.get(17)[2];
         int d = fishSet.get(17)[3]; // shark direction
 
-        int tx = x + dx[d];
-        int ty = y + dy[d];
-        boolean check = false;
+        moveFish(map, fishSet);
 
-        while (tx >= 0 && tx < 4 && ty >= 0 && ty < 4){
-            if (map[tx][ty] == 0) {
-                tx += dx[d];
-                ty += dy[d];
-                continue;
-            }
-
+        for (int i = 0; i < 3; i++) {
+            tx += dx[d];
+            ty += dy[d];
+            if (tx < 0 || tx >= 4 || ty < 0 || ty >= 4) break;
+            if (map[tx][ty] == 0) continue;
+            
             int[][] mapCpy = new int[4][];
-            for (int i = 0; i < 4; i++) {
-                mapCpy[i] = map[i].clone();
-            }
-
             ArrayList<int[]> fishSetCpy = new ArrayList<>();
-            for (int i = 0; i < fishSet.size(); i++) {
-                fishSetCpy.add(fishSet.get(i).clone());
-            }
+
+            for (int j = 0; j < 4; j++) mapCpy[j] = map[j].clone(); // map clone
+            for (int j = 0; j < fishSet.size(); j++) fishSetCpy.add(fishSet.get(j).clone()); // fishInfo clone
 
             int fishIdx = mapCpy[tx][ty];
             int[] shark = fishSetCpy.get(17);
-            mapCpy[tx][ty] = 17;
-            mapCpy[shark[1]][shark[2]] = 0;
+            mapCpy[tx][ty] = 17; // 상어 위치 update
+            mapCpy[shark[1]][shark[2]] = 0; // 빈자리 0 update
+            // shark info update
             shark[1] = tx;
             shark[2] = ty;
             shark[3] = fishSetCpy.get(fishIdx)[3];
 
-            dfs(mapCpy, fishSetCpy, dinner + fishIdx,depth + 1);
-
-            check = true;
-            tx += dx[d];
-            ty += dy[d];
+            dfs(mapCpy, fishSetCpy, dinner + fishIdx);
         }
-
-        if (!check) // 물고기 먹을 자리가 없다면 집으로 가야함
-        {
-//            System.out.println("return " + dinner);
-            max = (max < dinner) ? dinner : max;
-        }
+        
+        max = (max < dinner) ? dinner : max;
     }
 
     static void moveFish(int[][] map, ArrayList<int[]> fishSet){
+        // 16마리의 fish 탐색
         for (int i = 1; i <= 16 ; i++) {
             int[] fish = fishSet.get(i);
-            if (map[fish[1]][fish[2]] != i){
-                continue;
-            }
+            if (map[fish[1]][fish[2]] != i) continue; // 이미 먹은 물고기를 만났다면
             for (int j = 0; j < 8; j++) {
                 int d = (j + fish[3]) % 8;
                 int tx = fish[1] + dx[d];
@@ -112,7 +75,7 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception{
-  //      System.setIn(new FileInputStream("src/SSA/input.txt"));
+//        System.setIn(new FileInputStream("src/SSA/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         for (int i = 0; i < 4; i++) {
@@ -120,27 +83,19 @@ public class Main {
             for (int j = 0; j < 4; j++) {
                 int fishNum = Integer.parseInt(st.nextToken());
                 int d = Integer.parseInt(st.nextToken()) - 1;
-                fishInfo.add(new int[]{fishNum, i,j,d});
+                fishInfo.add(new int[]{fishNum, i, j, d});
                 arr[i][j] = fishNum;
             }
         }
-        fishInfo.add(new int[]{0,0,0,0});
-        fishInfo.add(new int[]{17, 0,0,0}); // 상어
+        fishInfo.add(new int[]{0,0,0,0}); // dummy
+        fishInfo.add(new int[]{17, 0,0,0}); // 상어위치 초기화
         fishInfo.sort((o1, o2)->Integer.compare(o1[0], o2[0]));
 
         int fishIdx = arr[0][0];
         arr[0][0] = 17;
-        fishInfo.get(17)[1] = fishInfo.get(17)[2] = 0;
         fishInfo.get(17)[3] = fishInfo.get(fishIdx)[3];
-        dfs(arr, fishInfo,  fishIdx, 0 );
+        dfs(arr, fishInfo,  fishIdx);
 
         System.out.println(max);
     }
-
 }
-/*
-7 6 2 3 15 6 9 8
-3 1 1 8 14 7 10 1
-6 1 13 6 4 3 11 4
-16 1 8 7 5 2 12 2
- */
