d9a79187e563d62ab7a188c6785f2105b6f87202
slcloe <67732600+slcloe@users.noreply.github.com>
2023-08-30 16:11:31 +0900

    [Silver II] Title: 이동하기, Time: 584 ms, Memory: 79100 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][] arr;
+    static int R, C;
+    static int[][] dp;
+    static int[] dx = {1, 0, 1};
+    static int[] dy = {0, 1, 1};
+
+    static boolean isRange(int x, int y){
+        return x >= 0 && x < R && y >= 0 && y < C;
+    }
+
+    static void dfs(int x, int y){
+
+        for (int i = 0; i < dx.length; i++) {
+            int tx = x - dx[i];
+            int ty = y - dy[i];
+            if (!isRange(tx, ty)) continue;
+            int tmp = arr[x][y] + dp[tx][ty];
+            dp[x][y] = (dp[x][y] < tmp) ? tmp : dp[x][y];
+        }
+
+        for (int i = 0; i < dx.length; i++) {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (!isRange(tx, ty)) continue;
+            dfs(tx, ty);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(),  " ");
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        arr = new int[R][C];
+        dp = new int[R][C];
+
+        for (int i = 0; i < R; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < C; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][0] = arr[0][0];
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                for (int k = 0; k < dx.length; k++) {
+                    int tx = i - dx[k];
+                    int ty = j - dy[k];
+                    if (!isRange(tx, ty)) continue;
+                    int tmp = arr[i][j] + dp[tx][ty];
+                    dp[i][j] = (dp[i][j] < tmp) ? tmp : dp[i][j];
+                }
+            }
+        }
+        System.out.println(dp[R - 1][C - 1]);
+    }
+}
