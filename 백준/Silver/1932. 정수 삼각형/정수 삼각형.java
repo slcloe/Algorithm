2b2a5d1e6d7317d055601f5fc60e1f8e63a7f640
slcloe <67732600+slcloe@users.noreply.github.com>
2023-09-13 23:02:59 +0900

    [Silver I] Title: 정수 삼각형, Time: 280 ms, Memory: 26628 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -26,12 +26,16 @@ public class Main {
                 int b = 0;
                 if (j - 1 >= 0) b = dp[i - 1][j - 1];
                 dp[i][j] = Math.max(a, b) + triangle[i][j];
-                max = Math.max(dp[i][j], max);
+                if (N - 1 == i)
+                    max = Math.max(dp[i][j], max);
             }
         }
 //        for (int[] arr: dp){
 //            System.out.println(Arrays.toString(arr));
 //        }
+        for (int j = 0;j < N;j++) {
+            if (dp[N - 1][j] > max) max = dp[N][j];
+        }
         System.out.println(max);
     }
 
