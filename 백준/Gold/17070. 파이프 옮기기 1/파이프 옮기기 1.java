059e2fb02054f1aafaa2ec5b224eed941c988e66
slcloe <67732600+slcloe@users.noreply.github.com>
2023-10-12 13:24:57 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,54 +1,41 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     static int N;
-    static int[][] arr;
-
-    static int[] dx = {0, 1, 1};
-    static int[] dy = {1, 0, 1};
-
-    // 방향별 갈 수 있는 방향 idx 정의
-    static int[][] direct = {
-            {0, 2},
-            {1, 2},
-            {0, 1, 2}
-    };
-    static int result;
-
-    static void dfs(int x, int y, int d) // 현재 pos (x, y) 방향 d ( 0 = 가로 , 1 = 세로, 2 = 대각 )
-    {
-        // 목적지에 도달했을 경우 return
-        if (x == N - 1 && y == N - 1){
-            result++; return;
-        }
-
-        for (int i = 0; i < direct[d].length; i++) {
-            int tx = x + dx[direct[d][i]];
-            int ty = y + dy[direct[d][i]];
-            if (tx < 0 || tx >= N || ty < 0 || ty >= N) continue;
-            if (arr[tx][ty] == 1) continue;
-            if (direct[d][i] == 2){ // 가려고 하는 방향이 대각선일 경우
-                if (arr[tx - 1][ty] == 1 || arr[tx][ty - 1] == 1) continue; // 가로 & 세로 방향도 갈 수 있는지 체크해야함.
-            }
-            dfs(tx, ty, direct[d][i]);
-        }
-    }
-
+    static char[][] arr;
+    static int[][][] dp;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
+        arr = new char[N][N];
+        dp = new int[N][N][3];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = st.nextToken().charAt(0);
             }
         }
-        dfs(0, 1, 0);
-        System.out.println(result);
+
+        for (int i = 1; i < N && arr[0][i] == '0'; i++) {
+            dp[0][i][0] = 1;
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 1; j < N; j++) {
+                if (arr[i][j] == '1') continue;
+                if (arr[i - 1][j] == '0' && arr[i][j - 1] == '0'){
+                    dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
+                }
+                dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
+                dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
+            }
+        }
+
+        System.out.println(dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2]);
+        br.close();
     }
 }
