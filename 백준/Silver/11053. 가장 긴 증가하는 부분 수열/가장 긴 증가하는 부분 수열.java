ad693ddc69b8fa5a3d43ce0202061962ac594c58
slcloe <67732600+slcloe@users.noreply.github.com>
2024-08-04 22:45:51 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 108 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -8,17 +8,33 @@ public class Main {
     static int N;
     static int[] arr;
     static int[] arrb;
-    static int curSize;
     static int MaxSize;
+    static int[] result;
+
+    static void makeResult(int idx) {
+        int j = MaxSize;
+        result = new int[MaxSize + 1];
+        result[j] = arr[idx];
+
+//        System.out.println("MaxSize :" + (MaxSize + 1));
+
+        for(int i = idx - 1; i >= 0; i--) {
+            int pos = Arrays.binarySearch(result, arr[i]);
+            if (pos < 0) {
+                pos *= -1;
+                System.out.println(pos);
+                result[--pos] = arr[i];
+            }
+        }
+    }
     static void binarySearch(int idx){
-        if (arrb[MaxSize] < arr[idx]){
+        if (arrb[MaxSize] < arr[idx]) {
             arrb[++MaxSize] = arr[idx];
-            curSize = MaxSize;
+//            makeResult(idx);
             return;
         }
         if (arrb[0] > arr[idx]){
             arrb[0] = arr[idx];
-            curSize = 0;
             return;
         }
         int front = 0;
@@ -32,18 +48,14 @@ public class Main {
                 rear = middle - 1;
             }
             else{
-                curSize = middle;
                 return;
             }
         }
-//        System.out.println(front + " " + rear);
-        arrb[front] = arr[idx];
-        curSize = front;
-//        if (arrb[front] == arr[idx])
-//            curSize = front;
-//        else{
-//
-//        }
+        int i = Arrays.binarySearch(arrb, 0, MaxSize+1, arr[idx]);
+        i *= -1;
+
+        arrb[--i] = arr[idx];
+//        System.out.println(i + " " + front);
     }
 
     public static void main(String[] args) throws Exception{
@@ -59,13 +71,23 @@ public class Main {
         arrb[0] = arr[0];
         for (int i = 1; i < N; i++) {
             binarySearch(i);
-//            System.out.println(Arrays.toString(arrb));
+//            System.out.println(Arrays.toString(arrb) + " " + MaxSize);
         }
         System.out.println(MaxSize + 1);
-
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < MaxSize + 1; i++) {
+//            sb.append(result[i]).append(' ');
+////            System.out.print(arrb[i] + " ");
+//        }
+//        System.out.println(sb.toString());
     }
 }
 /*
 10
 8 2 4 3 6 11 7 10 14 5
+
+
+13
+3 4 5 6 2 3 1 7 4 3 5 6 7
+
  */
